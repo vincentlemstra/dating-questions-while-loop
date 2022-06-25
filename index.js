b5e939b473a0d33d11b5b3d2944d2335c86861bb
Vincent Lemstra <vincentlemstra@gmail.com>
2022-06-25 12:40:14 +0000

    Add correct way of showing answers

--- index.js
+++ index.js
@@ -4,45 +4,38 @@
 const questions = [
   "What is your first name?",
   "What is your last name?",
-  "What is your age?",
-  "Where do you live?",
-  "What is your gender?",
-  "Which genders are you interested in dating?",
-  "What is your main hobby?",
-  "What is your favourite movie?"
+  // "What is your age?",
+  // "Where do you live?",
+  // "What is your gender?",
+  // "Which genders are you interested in dating?",
+  // "What is your main hobby?",
+  // "What is your favourite movie?"
 ];
 
 const answers = [];
 let i = 0;
 
-console.log (`questions length: ${questions.length}`);
-console.log (`answers length: ${answers.length}`);
+console.log(`questions length: ${questions.length}`);
+console.log(`answers length: ${answers.length}`);
 
 while (answers.length < questions.length) {
   const question = questions[i];
   const answer = prompt(question);
 
+  // answers can't be shorter than 2 characters
+  if (answer.length < 3) {
+    console.log("This answer is too short");
+    continue;
+  }
   // Save the answer to variable 'answers'
   answers.push(answer);
   i++;
 }
 
-// Prints all questions 
-// console.log(questions);
-
-// Prints all questions in a table with correct index
-// console.table(questions);
-
-// Prints a specific question 
-// console.log(questions[3]);
-
-// Prompts a specific question
-// let awnser3 = prompt (questions[3]);
-// console.log (awnser3);
-
-
-
-
-
-
+// print 2 rows: the question, with the corrisponding answer.
+for (let i = 0; i < answers.length; i++) {
+  const humanNumber = i + 1;
+  console.log(`Question ${humanNumber}: ${questions[i]}`);
+  console.log(`Answer: ${answers[i]}`);
+}
 
